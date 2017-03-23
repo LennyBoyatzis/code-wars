4f7998fc3657f2c32eb14a44c76bad8c4eab89d2
Lenny Boyatzis <lennyboyatzis@gmail.com>
2017-03-23 19:30:34 +1100

    rd 2

--- digital-root.py
+++ digital-root.py
@@ -1,5 +1,6 @@
 def digital_root(n):
-    return n if n < 10 else digital_root(sum(map(int,str(n))))
+    return sum(map(lambda n: int(n), str(n)))
+    # return n if n < 10 else digital_root(sum(map(int,str(n))))
 
 if __name__ == "__main__":
-    print("Root --->", digital_root(154534))
+    print(digital_root(154534))

153b6cf8bf4014ab6d04bc6f96c3ae9ab4e0c371
Lenny Boyatzis <lennyboyatzis@gmail.com>
2017-04-04 16:31:24 +1000

    equal-sides-of-an-array

---
+++ equal-sides-of-array.py
@@ -0,0 +1,9 @@
+def find_even_index(arr):
+    left_side = lambda i: sum(arr[:i + 1])
+    right_side = lambda i: sum(arr[i:])
+
+    return [i for i, val in enumerate(arr) if left_side(i) == right_side(i)][0]
+
+if __name__ == "__main__":
+    even_index = find_even_index([1,2,3,2,1])
+    print("even index --->", even_index)

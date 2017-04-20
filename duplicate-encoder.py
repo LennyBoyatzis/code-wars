ada8c50f35aa4e287eec103812dc0caaa4b64319
Lenny Boyatzis <lennyboyatzis@gmail.com>
2017-04-20 10:56:08 +1000

    adding timeit module to test performance

--- duplicate-encoder.py
+++ duplicate-encoder.py
@@ -1,11 +1,36 @@
-def duplicate_encode(word):
-    char_more_than_once = lambda char: word.count(char) > 1 
-    return ''.join([')' if char_more_than_once(char) else '(' for char
-                    in word.lower()])
+# Because count is being used 
+# We have a loop within a loop 
+# This makes it an O(n^2) method
+# This method is concise and readable but could be more efficient
+# really_long_test_string = ''.join(['a' for x in range(100)])
+really_long_test_string = 'adfajshdval;cj.vncv.,ndfa.kds,nfasd.kjfhnad.kcj,ndk.jfhndsk.'
+
+def duplicate_encode_0n2(word):
+    word = word.lower()
+    return ''.join(['(' if word.count(x) else ')' for x in word])
+
+# The following is an O(n) method
+
+import collections
+
+def duplicate_encode_0n(word):
+    new_string = ''
+    word = word.lower()
+    d = collections.defaultdict(int)
+    for c in word:
+        d[c] += 1
+    for c in word:
+        new_string = new_string + ('(' if d[c] == 1 else ')')
+    return new_string
 
 if __name__ == "__main__":
-    print(duplicate_encode("din"))
-    print(duplicate_encode("recede"))
-    print(duplicate_encode("Success"))
-    print(duplicate_encode("(( @"))
+    import timeit
+
+    # Test performance - duplicate_encode_0n2
+    print(timeit.timeit("duplicate_encode_0n2(really_long_test_string)",
+                        setup="from __main__ import duplicate_encode_0n2, really_long_test_string"))
+
+    # Test performance - duplicate_encode_0n
+    print(timeit.timeit("duplicate_encode_0n(really_long_test_string)",
+                        setup="from __main__ import duplicate_encode_0n, really_long_test_string"))
 

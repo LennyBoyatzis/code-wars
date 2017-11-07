07dd7cdedd1822fa50ea4cc07e48cd4111a11c27
Lenny Boyatzis <lennyboyatzis@gmail.com>
2017-11-08 10:49:24 +1100

    concurrency vs parallelism in py

---
+++ parallel-vs-concurrency-in-py/serial-squirrel.py
@@ -0,0 +1,15 @@
+import time
+from websites import WEBSITE_LIST
+from utils import ping_website
+from utils import notify_owner
+from utils import check_website
+
+start_time = time.time()
+
+for address in WEBSITE_LIST:
+    check_website(address)
+
+end_time = time.time()
+
+print("Time for SerialSquirrel: %ssecs" % (end_time - start_time))
+
---
+++ parallel-vs-concurrency-in-py/threaded-squirrel.py
@@ -0,0 +1,31 @@
+import time
+from queue import Queue
+from threading import Thread
+
+NUM_WORKERS = 4
+task_queue = Queue()
+
+def worker():
+    while True:
+        address = task_queue.get()
+        check_website(address)
+        task_queue.task_done()
+
+start_time = time.time()
+
+# Create the worker threads
+threads = [Thread(target=worker) for _ in range(NUM_WORKERS)]
+
+# Add the websites to the task queue
+[task_queue.put(item) for item in WEBSITE_LIST]
+
+# Start all the workers
+[thread.start() for thread in threads]
+
+# Wait for all the tasks in the queue to be processed
+task_queue.join()
+
+end_time = time.time()
+
+print("Time for ThreadedSquirrel: %ssecs" % (end_time - start_time))
+
